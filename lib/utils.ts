9fb07750fa4812cbe6fa060183b6a415ac205436
dhananjaybhatia <dhananjaybhatia@yahoo.co.in>
2025-07-29 15:17:35 +1000

    Form Page designed and implemented. Form and resume upload is working.

---
+++ lib/utils.ts
@@ -0,0 +1,9 @@
+export function formatSize(bytes: number): string {
+    if (bytes === 0) return "0 Bytes";
+
+    const sizes = ["Bytes", "KB", "MB", "GB", "TB"];
+    const i = Math.floor(Math.log(bytes) / Math.log(1024));
+    const size = bytes / Math.pow(1024, i);
+
+    return `${size.toFixed(2)} ${sizes[i]}`;
+}
